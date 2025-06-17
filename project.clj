78ddea26134debdf1a595c93033a3eabe43d6fdf
Matt Bowcock <matt.bowcock@gmail.com>
2025-06-17 12:26:14 -0400

    Updates to dependency versions

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]
-                 [clj-http "1.0.1"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/data.json "2.5.1"]
+                 [clj-http "3.13.0"]]
   :main geo-clj.core)
