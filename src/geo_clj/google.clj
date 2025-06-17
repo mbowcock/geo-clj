10d8cd0c6a6d4445f61937f26a9fe2ec91227270
Matt Bowcock <matt.bowcock@gmail.com>
2025-06-17 12:50:48 -0400

    refactor the geocode function

--- src/geo_clj/google.clj
+++ src/geo_clj/google.clj
@@ -4,15 +4,14 @@
 
 (def url "https://maps.googleapis.com/maps/api/geocode/json")
 
-;; todo - remove url from here
 (defn geocode [address]
   "Uses google geocode api to geocode address. Returns lat / lng."
-  (let [apikey (System/getenv "GOOGLE_APIKEY")]
-        (:location
-          (:geometry 
-            (first 
-              (:results 
-                (json/read-str
-                  (:body
-                    (client/get url
-                                {:query-params {"key" apikey "address" address}})) :key-fn keyword)))))))
+  (let [apikey (System/getenv "GOOGLE_APIKEY")
+        data (:body (client/get url {:query-params {"key" apikey "address" address}}))
+        json-data (json/read-str data :key-fn keyword)]
+    (->>
+     json-data
+     :results
+     first
+     :geometry
+     :location)))
